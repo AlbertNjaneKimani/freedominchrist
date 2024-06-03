e651e28b1323b9f13ac1e0509bb25e129c3e029e
AlbertNjaneKimani <albertnjanek@gmail.com>
2024-06-03 14:25:49 +0300

    completed project

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Create and activate a virtual environment
-python3.10 -m venv venv
+python3.9 -m venv venv
 source venv/bin/activate
 
 # Upgrade pip and install the project's dependencies
@@ -13,9 +13,9 @@ pip install -r requirements.txt
 
 # Apply database migrations
 echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
