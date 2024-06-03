9cf296d47d372c479136cdddf381d0a96a7da086
AlbertNjaneKimani <albertnjanek@gmail.com>
2024-06-03 13:20:50 +0300

    project freedominchrist

--- build.sh
+++ build.sh
@@ -4,18 +4,18 @@
 set -e
 
 # Create and activate a virtual environment
-python3.9 -m venv venv
+python -m venv venv
 source venv/bin/activate
 
 # Upgrade pip and install the project's dependencies
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+ pip install --upgrade pip
+ pip install -r requirements.txt
 
 # Apply database migrations
 echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
